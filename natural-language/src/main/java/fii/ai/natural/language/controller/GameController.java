8f5dc344079a07697a9e5b6c0af95507ebf91938
stefan-adrian <adrian.stefan@info.uaic.ro>
2018-11-28 02:45:42 +0200

    Created game service strucutre and implemented sub-problem creation

--- natural-language/src/main/java/fii/ai/natural/language/controller/GameController.java
+++ natural-language/src/main/java/fii/ai/natural/language/controller/GameController.java
@@ -3,6 +3,7 @@ package fii.ai.natural.language.controller;
 import fii.ai.natural.language.input.InputTree;
 import fii.ai.natural.language.mapper.TreeMapper;
 import fii.ai.natural.language.model.MovesTree;
+import fii.ai.natural.language.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,13 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GameController {
-    public TreeMapper treeMapper;
+    private TreeMapper treeMapper;
+    private GameService gameService;
 
     @Autowired
-    public GameController(TreeMapper treeMapper) {
+    public GameController(TreeMapper treeMapper, GameService gameService) {
         this.treeMapper = treeMapper;
+        this.gameService = gameService;
     }
 
+    /**
+     * This method maps the inputTree to a MovesTree and after gonna call(when implemented) commentMovesTree method from gameService
+     * to generate comments for the inputTree
+     * @param inputTree represents the input that is a complete chess game
+     * @return the return value gonna be changed to a List of comments
+     */
     @PostMapping("/game")
     public MovesTree commentGame(@RequestBody InputTree inputTree) {
         return treeMapper.map(inputTree);
---
+++ natural-language/src/main/java/fii/ai/natural/language/service/GameService.java
@@ -0,0 +1,45 @@
+package fii.ai.natural.language.service;
+
+import fii.ai.natural.language.model.MoveVariant;
+import fii.ai.natural.language.model.MovesTree;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+/**
+ * Here I tried to create a structure for the service that gets the input(mapped to model) from the controller and
+ * decorates the model with metadata and comments, in the scope to return a list of comments to the controller.
+ */
+@Service
+public class GameService {
+
+
+    /**
+     * This should be called from the controller and decorate the moves tree first with metadata and
+     * after that should generate a list of comments for the chess game(first should decorate the moves
+     * three with comments individually for every move and after that should put them together)
+     * @param movesTree represents the input from AI module mapped to MovesTree object
+     * @return the return value gonna be changed in a list of comments
+     */
+    public MovesTree commentMovesTree(MovesTree movesTree) {
+        decorateMovesTree(movesTree);
+        //TODO call method that generates the comments for the movesTree and return comments List.
+        return movesTree;
+    }
+
+    private void decorateMovesTree(MovesTree movesTree) {
+        for (int index = 0; index < movesTree.getMainVariant().getMoves().size(); index++) {
+            MovesTree partialMovesTree = getMovesTreeUpToGivenIndex(movesTree, index);
+            // TODO create and call method that does the decoration of the partialMovesTree with metadata(generate metadata for sub-problem)
+        }
+    }
+
+    private MovesTree getMovesTreeUpToGivenIndex(MovesTree movesTree, int index) {
+        MovesTree partialMovesTree = new MovesTree();
+        partialMovesTree.setMainVariant(new MoveVariant());
+        partialMovesTree.setInitialStateFEN(movesTree.getInitialStateFEN());
+        partialMovesTree.getMainVariant().setMoves(new ArrayList<>(movesTree.getMainVariant().getMoves()));
+        partialMovesTree.getMainVariant().getMoves().subList(index + 1, partialMovesTree.getMainVariant().getMoves().size()).clear();
+        return partialMovesTree;
+    }
+}
