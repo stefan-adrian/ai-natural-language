068b0905c5a09af4c55079d28f979f4fc7c20ea2
florentinatanasiu <florentina.tanasiu.13@gmail.com>
2018-11-30 14:37:42 +0200

    Add files via upload

---
+++ natural-language/src/main/java/fii/ai/natural/language/model/MovePosition.java
@@ -0,0 +1,32 @@
+package fii.ai.natural.language.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import fii.ai.natural.language.model.Node;
+
+/**
+ *  This is the model for the input only with FEN position and possible moves from that position
+ */
+
+public class MovePosition{
+
+    /**
+     * Initial position described FEN notation
+     */
+    private String initialStateFEN;
+
+    /**
+     * List of possible moves from the given position
+     */
+    private List<Node> variants = new ArrayList<>();
+    
+
+    public String getInitialStateFEN() { return initialStateFEN; }
+
+    public void setInitialStateFEN(String initialStateFEN) { this.initialStateFEN = initialStateFEN; }
+
+    public List<Node> getVariants() { return variants; }
+
+    public void setVariants(List<Node> variants) { this.variants = variants; }
+
+}
